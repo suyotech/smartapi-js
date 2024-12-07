2c4f302eee8132cbe147837ce995e959aa420b50
suyogbpatil <suyogbpatil@yahoo.com>
2024-12-07 12:19:27 +0530

    removed old websocket classed

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
 import SmartApi from "./SmartapiHttp.js";
 import SmartapiInstruments from "./SmartapiInstruments.js";
-import SmartApiWS20 from "./SmartapiWS.js";
 import HistoricalDataProvider from "./HistoricalDataProvider.js";
-import SmartapiWS30 from "./SmartapiWS30.js";
 import SmartapiWS40 from "./SmartapiWS40.js";
 
-export {
-  SmartApi,
-  SmartapiInstruments,
-  SmartApiWS20,
-  HistoricalDataProvider,
-  SmartapiWS30,
-  SmartapiWS40,
-};
+export { SmartApi, SmartapiInstruments, HistoricalDataProvider, SmartapiWS40 };
