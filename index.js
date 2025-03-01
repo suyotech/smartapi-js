7dbb174bfe8f6227232e9e39ae546bc4378da10c
suyogbpatil <suyogbpatil@yahoo.com>
2025-03-01 11:52:34 +0530

    updated index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-import SmartApi from "./lib/SmartapiHttp.js";
-import SmartapiInstruments from "./lib/SmartapiInstruments.js";
-import SmartapiWS40 from "./lib/SmartapiWS40.js";
-import HistoricalDataProvider from "./lib/HistoricalDataProvider.js";
-
-export { SmartApi, SmartapiInstruments, SmartapiWS40, HistoricalDataProvider };
