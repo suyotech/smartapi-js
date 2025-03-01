9ce626c7dca59d930795a02cb9cc20bae4e1d7b9
Suyog Patil <49726444+suyogbpatil@users.noreply.github.com>
2025-03-01 11:53:27 +0530

    Dev1 (#1)
    * updated erors catch
    
    * updated index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-import SmartApi from "./SmartapiHttp.js";
-import SmartapiInstruments from "./SmartapiInstruments.js";
-import HistoricalDataProvider from "./HistoricalDataProvider.js";
-import SmartapiWS40 from "./SmartapiWS40.js";
-
-export { SmartApi, SmartapiInstruments, HistoricalDataProvider, SmartapiWS40 };
